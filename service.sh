b3215ddf9235afe34d479410bd47292e94de03c8
wei.hu <wei.hu@transwarp.io>
2016-03-02 20:27:05 +0800

    refine 	deleted:    docker-compose.yml 	new file:   docker-compose.yml.j2 	new file:   j2.py 	new file:   release.config 	modified:   service.sh 	new file:   trunk.config

--- service.sh
+++ service.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.yml "$@"
+I=1
+
+CONFIG="trunk.config"
+
+RES_ARGS=""
+
+while [ $I -le $# ]; do
+  S=1
+  eval "PEEK=\${$I}"
+  case "$PEEK" in
+    -config)
+        I=$(expr $I + 1)
+        eval "VAL=\${$I}"
+        CONFIG="$VAL"
+        ;;
+    *)
+        RES_ARGS="$RES_ARGS $PEEK"
+        ;;
+  esac
+  I=$(expr $I + $S)
+done
+
+
+./j2.py --config $CONFIG docker-compose.yml.j2
+
+docker-compose -f docker-compose.yml $RES_ARGS
